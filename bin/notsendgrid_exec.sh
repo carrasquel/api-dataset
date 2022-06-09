c7a57bd1e27ab1657fb54fe7d65780723ed26795
Nelson Carrasquel <carrsquel@outlook.com>
2022-06-09 00:01:41 -0400

    users in sh

--- bin/notsendgrid_exec.sh
+++ bin/notsendgrid_exec.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/local/bin/docker-entrypoint.sh postgres & python3 /opt/api/app.py && fg
+su - postgres /usr/local/bin/docker-entrypoint.sh postgres & 
+su - root python3 /opt/api/app.py && fg
